67fbb95225372e54be3e4c5695749fc1e75f1d76
Cinthia Pardos <cinthiapardos@gmail.com>
2024-11-25 22:50:02 -0300

    fchbd

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-
-import './App.css'
+import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-
 function App() {
-
   return (
     <>
- <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route index path="/login" element={<div>login</div>} />
-          <Route index path="/register/:token" element={<h1>ASFDsgdfgsrd</h1>} />
+          <Route
+            index
+            path="/register/:token"
+            element={<h1>ASFDsgdfgsrd</h1>}
+          />
           {/**/}
-
-        
-          <Route path="*" element={<div>4025</div> } />
+          <Route path="/*" element={<div>4025</div>} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
